2a58e53a80a433de31292d36063c500b016c1753
atalw <akshitalwar11@gmail.com>
2022-04-01 13:47:31 +0530

    6 improv

--- code/6.zigzag-conversion.rs
+++ code/6.zigzag-conversion.rs
@@ -13,61 +13,24 @@ impl Solution {
 	// ABC 2
 	// AC B
 	// ABCDEF 5
-	// A BF CE D E
+	// A B C DF E
+	// ABCDEFGHIJ 5
+	// AJ BI CH DF E
     pub fn convert(s: String, num_rows: i32) -> String {
-		let s = "ABCDEF".to_string();
-		let num_rows = 5;
-		let mut res = "".to_string();
-		let mut count = if num_rows % 2 == 0 { num_rows + 1 } else { num_rows };
-		let chars: Vec<char> = s.chars().collect();
-		let mut odd_vals: Vec<i32> = Vec::new();
-		for i in 0..num_rows {
-			if i % 2 != 0 {
-				odd_vals.push(i);	
-			}
+		let mut strings = vec![String::new(); num_rows as usize];
+		let mut i = 0;
+		let mut down = true;
+
+		if num_rows < 2 {
+			return s
 		}
-		// println!("{:?}", odd_vals);
-		if num_rows == 1 { return s }
-		if num_rows == s.len() as i32 { return s }
-		count = if num_rows == 2 { 1 } else { count };
-		for i in 0..num_rows {
-			match i {
-				0 => { // first index
-					let mut j = i;
-					while j < s.len() as i32 {
-						res.push(chars[j as usize]);
-						j = j+count+1;
-					}
-				},
-				i if i % (num_rows-1) == 0 => { // last index
-					let mut j = i;
-					while j < s.len() as i32 {
-						res.push(chars[j as usize]);
-						j = j+count+1;
-					}
-				},
-				i if (i+1) % 2 == 0 => {
-					let mut j = i;
-					let mut index = odd_vals.len()-1;
-					while j < s.len() as i32 {
-						res.push(chars[j as usize]);
-						j = j+odd_vals[index]+1;
-						index += 1;
-						index = if index % odd_vals.len() == 0 { 0 } else { index }; // loop index
-					}
-				},
-				_ => {
-					let mut j = i;
-					let mut index = 0;
-					while j < s.len() as i32 {
-						res.push(chars[j as usize]);
-						j = j+odd_vals[index]+1;
-						index += 1;
-						index = if index % odd_vals.len() == 0 { 0 } else { index }; // loop index
-					}
-				}
-			}
+
+		for c in s.chars() {
+			strings[i].push(c);
+			i = if down { i + 1 } else { i - 1 };
+			down = down == { i > 0 && i < num_rows as usize - 1 };
 		}
-		res
+
+		strings.concat()
     }
 }
