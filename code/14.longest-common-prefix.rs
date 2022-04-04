3bd616336568b58d9a28933f1f8e3c2bc9aed701
atalw <akshitalwar11@gmail.com>
2022-04-04 13:19:42 +0530

    14 functional

--- code/14.longest-common-prefix.rs
+++ code/14.longest-common-prefix.rs
@@ -5,22 +5,38 @@ impl Solution {
 		// [lamp, ramp, map] = ""
 		// [map, map, map] = map
 		
-		let mut prefix = "".to_string();
-		let mut idx = 0;
 
-		while idx < strs[0].len() {
-			let letter = strs[0].chars().nth(idx).unwrap();
-			for s in strs.iter().skip(1) {
-				match s.chars().nth(idx) {
-					Some(c) if c == letter => { continue },
-					_ => return prefix
-				}
+		match strs.is_empty() {
+			true => "".to_string(),
+			_ => {
+				strs.iter().skip(1).fold(strs[0].clone(), |acc, x| {
+					acc
+						.chars()
+						.zip(x.chars())
+						.take_while(|(x, y)| x == y)
+						.map(|(x, _)| x)
+						.collect()
+				})
 			}
-			prefix.push(letter);
-			idx += 1;
 		}
 
 
-		prefix
+		// let mut prefix = "".to_string();
+		// let mut idx = 0;
+
+		// while idx < strs[0].len() {
+		//     let letter = strs[0].chars().nth(idx).unwrap();
+		//     for s in strs.iter().skip(1) {
+		//         match s.chars().nth(idx) {
+		//             Some(c) if c == letter => { continue },
+		//             _ => return prefix
+		//         }
+		//     }
+		//     prefix.push(letter);
+		//     idx += 1;
+		// }
+
+
+		// prefix
     }
 }
