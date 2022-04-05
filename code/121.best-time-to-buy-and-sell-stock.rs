90b24b1c605b193610b3d61c08674e98a7384b44
atalw <akshitalwar11@gmail.com>
2022-04-05 16:58:20 +0530

    121 functional

--- code/121.best-time-to-buy-and-sell-stock.rs
+++ code/121.best-time-to-buy-and-sell-stock.rs
@@ -1,13 +1,17 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-		let mut profit = 0;
 		let mut min_price = prices[0];
 
-		for price in prices {
-			profit = profit.max(price - min_price);
+		prices.iter().fold(0, |profit, &price| {
 			min_price = min_price.min(price);
-		}
+			profit.max(price - min_price)
+		})
 
-		profit
+		// for price in prices {
+		//     profit = profit.max(price - min_price);
+		//     min_price = min_price.min(price);
+		// }
+
+		// profit
     }
 }
