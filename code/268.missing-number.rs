359307fe7008bbb6d1777a712d02b7fc33efd577
atalw <akshitalwar11@gmail.com>
2022-04-08 14:07:03 +0530

    268 xor

--- code/268.missing-number.rs
+++ code/268.missing-number.rs
@@ -4,17 +4,32 @@ use std::iter::FromIterator;
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
         // start from 0, keep adding 1, take num from array
-
-		let mut set: HashSet<i32> = HashSet::from_iter(nums.iter().cloned());
-
+		
 		let mut n = 0;
-		while !set.is_empty() {
-			match set.take(&n) {
-				Some(val) => n += 1,
-				None => return n as i32
-			}
+		let mut i = 0;
+		
+		// ratt lo formula wali vibes
+		// this works on paper but how was it derived?
+		// https://leetcode.com/problems/missing-number/discuss/69791/4-Line-Simple-Java-Bit-Manipulate-Solution-with-Explaination
+		while i < nums.len() {
+			// n ^= nums[i] ^ (i as i32 + 1);
+			n = n ^ nums[i] ^ i as i32; 
+			i += 1;
 		}
 
-		return nums.len() as i32
+		// important final xor
+		n ^ i as i32
+
+		// let mut set: HashSet<i32> = HashSet::from_iter(nums.iter().cloned());
+
+		// let mut n = 0;
+		// while !set.is_empty() {
+		//     match set.take(&n) {
+		//         Some(val) => n += 1,
+		//         None => return n as i32
+		//     }
+		// }
+
+		// return nums.len() as i32
     }
 }
