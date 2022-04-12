3be26e200fe741fdbc5a8d0ca5ba704d49e9ed53
atalw <akshitalwar11@gmail.com>
2022-04-12 19:55:23 +0530

    139

---
+++ code/139.word-break.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+impl Solution {
+	// https://youtu.be/Sx9NNgInc3A
+    pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
+
+		let mut dp: HashMap<usize, bool> = HashMap::new();
+		dp.insert(s.len(), true);
+
+		for i in (0..s.len()).rev() {
+			for word in &word_dict {
+				let end = i + word.len();
+				match (s.get(i..end), dp.get(&end)) {
+					(Some(a), Some(&b)) if a == word => { dp.insert(i, b); },
+					_ => {}
+					
+				}
+			}
+		}
+
+		match dp.get(&(0 as usize)) {
+			Some(&val) => val,
+			None => false
+		}
+	}
+}
+
